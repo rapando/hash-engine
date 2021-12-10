c38ed6c57196d7116bbc40fcb8fd56cfea2b2518
rapando <samson.rapando@betika.com>
2021-12-10 16:57:33 +0300

    changes on permutations:

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"log"
+	"os"
 	"time"
 
 	"github.com/go-redis/redis/v8"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/rapando/hash-engine/md5engine"
+	"github.com/joho/godotenv"
 	"github.com/rapando/hash-engine/utils"
 )
 
@@ -25,9 +27,15 @@ func main() {
 	}()
 	var err error
 	var base base
+
+	err = godotenv.Load()
+	if err != nil {
+		log.Printf("unable to load dotenv because %v", err)
+		return
+	}
+
 	base.ctx = context.Background()
-	var dbURI = "sam:therealsam@tcp(localhost:3306)/hash_engine_db"
-	base.db, err = utils.DbConnect(dbURI)
+	base.db, err = utils.DbConnect(os.Getenv("DB_URI"))
 	if err != nil {
 		panic("db error")
 	}
@@ -38,32 +46,32 @@ func main() {
 		"mnbvcxz~!@#$%^&*()_+|}{POIUYTREWQASDFGHJKL:\"?><MNBVCXZ")
 	var n = len(chars)
 
-	var stopChan = make(chan bool)
-	for i := 1; i <= n; i++ {
-		section := chars[0:i]
-		go permute(base.ctx, section, 0, len(section)-1, base.cache)
-	}
-	go md5engine.MD5Subscriber(base.ctx, base.cache, base.db)
-	<-stopChan
+	// var stopChan = make(chan bool)
+	// for i := 1; i <= n; i++ {
+	// 	section := chars[0:i]
+	// 	go permute(base.ctx, section, 0, len(section)-1, base.cache)
+	// }
+	// go md5engine.MD5Subscriber(base.ctx, base.cache, base.db)
+	// <-stopChan
 }
 
-func permute(ctx context.Context, a []rune, l, r int, cache *redis.Client) {
-	var s string
-	if l == r {
-		s = string(a)
-		fmt.Println(s)
-		_ = cache.Publish(ctx, "passwords", s)
-	} else {
-		for i := l; i <= r; i++ {
-			temp := a[l]
-			a[l] = a[i]
-			a[i] = temp
-
-			permute(ctx, a, l+1, r, cache)
-
-			temp = a[l]
-			a[l] = a[i]
-			a[i] = temp
-		}
+func getNoOfPossibilities(length int) (possibilities int) {
+	if length == 1 {
+		return 1
 	}
+
+	for i := 1; i < length; i++ {
+		possibilities += length * factorial(i)
+	}
+	possibilities += factorial(length)
+	return possibilities
 }
+
+func factorial(n int) (f int) {
+	f = 1
+	for i := n; i >= 1; i-- {
+		f *= n
+	}
+	return f
+}
+func differentFlagPermutations()
